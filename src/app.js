319c6f35356e39220e7252dd2b79e0b62b42bab1
Tennobis <tanveer2004hossain@gmail.com>
2024-08-16 18:58:33 +0530

    bug fixes

--- src/app.js
+++ src/app.js
@@ -5,23 +5,9 @@ import { serverLimit } from "./constants";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
-app.use(
-  express.json({
-    limit: serverLimit,
-  })
-);
-app.use(
-  express.urlencoded({
-    extended: true,
-    limit: serverLimit,
-  })
-);
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(express.json({ limit: serverLimit }));
+app.use(express.urlencoded({ extended: true, limit: serverLimit }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
