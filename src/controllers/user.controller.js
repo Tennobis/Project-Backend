975c175451169d2c6528fb6be76d934c3fe6ec16
Tennobis <tanveer2004hossain@gmail.com>
2024-08-22 21:53:30 +0530

    minor changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({
-    message: "Check karle bhai",
+    message: "This is a test message",
   });
 });
 
