cea8316a4ddcf9d487d8c4098246751b2367e330
Tennobis <tanveer2004hossain@gmail.com>
2025-06-04 22:56:14 +0530

    added playlist delete and update functions in the controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -129,11 +129,51 @@ const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
 
 const deletePlaylist = asyncHandler(async (req, res) => {
   const { playlistId } = req.params;
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Invalid playlist id ");
+  }
+
+  const playlist = await Playlist.findById(playlistId);
+
+  if (!playlist) {
+    throw new ApiError(404, "Playlist not found");
+  }
+  if (playlist.owner.toString() !== req.user?._id.toString()) {
+    throw new ApiError(403, "Only playlist owner can delete the playlist");
+  }
+  await Playlist.findByIdAndDelete(playlistId);
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Playlist deleted successfully"));
 });
 
 const updatePlaylist = asyncHandler(async (req, res) => {
   const { playlistId } = req.params;
   const { name, description } = req.body;
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Invalid playlist id");
+  }
+  if (!name || !description) {
+    throw new ApiError(400, "Playlist name and description are required");
+  }
+  const playlist = await Playlist.findById(playlistId);
+
+  if (!playlist) {
+    throw new ApiError(404, "Playlist not found");
+  }
+  if (playlist.owner.toString() !== req.user?._id.toString()) {
+    throw new ApiError(403, "Only playlist owner can update the playlist ");
+  }
+  if (name) {
+    playlist.name = name;
+  }
+  if (description) {
+    playlist.description = description;
+  }
+  await playlist.save();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "Playlist updated successfully"));
 });
 
 export {
